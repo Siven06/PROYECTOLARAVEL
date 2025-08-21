e3af366b8251b391e0daee21aa1891abab8aa138
Siven06 <geyllermojica2006@gmail.com>
2025-08-21 17:08:34 -0500

    Botones/Delete/Action

---
+++ resources/views/usuario/delete.blade.php
@@ -0,0 +1,28 @@
+<div class="modal" id="modal-eliminar-01" role="dialog" arial-labelledby="exampleModalLabel">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content bg-danger">
+
+            <form action="" method="post">
+
+                <div class="modal-header">
+                    <h4>Eliminar Registro</h4>
+                </div>
+
+                <div class="modal-body">
+                    ¿Usted Desea Eliminar el registro xyz?
+                </div>
+
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-outline-light">Eliminar</button>
+                    <button type="button" class="btn btm-outline-light" data-bs-dismiss="modal">Cerrar</button>
+
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+
+</div>
